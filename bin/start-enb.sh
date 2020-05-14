fb84378c8f42dffcf21145a1cca698691a5d1443
prabodhmishra <35931371+prabodhmishra@users.noreply.github.com>
2020-05-14 17:58:17 -0400

    Fix enb.conf file path

--- bin/start-enb.sh
+++ bin/start-enb.sh
@@ -4,4 +4,4 @@ set -eux
 cd /local/openairinterface5g/ || exit
 source oaienv
 cd /local/openairinterface5g/cmake_targets || exit
-sudo -E ./lte_build_oai/build/lte-softmodem -O /local/enb.conf --nokrnmod 1 --noS1 --eNBs.[0].rrc_inactivity_threshold 0 2>&1 | tee /tmp/enb.log
+sudo -E ./lte_build_oai/build/lte-softmodem -O /local/etc/enb.conf --nokrnmod 1 --noS1 --eNBs.[0].rrc_inactivity_threshold 0 2>&1 | tee /tmp/enb.log
