eabee779da32b35bd98509fbff93b7509c07f534
Prabodh Mishra <pmishra@clemson.edu>
2020-05-16 00:22:08 -0400

    Use Node Type instead of node ID to find build type

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,11 @@
 # Run appropriate setup script
 
 NODE_ID=$(geni-get client_id)
+NODE_TYPE=${NODE_ID:0:3}
 
-if [ $NODE_ID = "rue1" ]; then
+if [ $NODE_TYPE = "rue" ]; then
     /local/repository/bin/build-ue.sh
-elif [ $NODE_ID = "enb1" ]; then
+elif [ $NODE_TYPE = "enb" ]; then
     /local/repository/bin/build-enb.sh
 else
     echo "no setup necessary"
