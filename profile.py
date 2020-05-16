606921b6e0e4ff859c16b82bd61966cc11f56010
prabodhmishra <35931371+prabodhmishra@users.noreply.github.com>
2020-05-15 23:05:01 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -21,6 +21,7 @@ connected via an attenuator matrix:
 
 * Intel NUC5300/B210 w/ OAI UE (`rue1`)
 * Intel NUC5300/B210 w/ OAI eNB (`enb1`)
+* Intel NUC5300/B210 w/ OAI eNB (`enb2`)
 
 """
 
@@ -71,12 +72,19 @@ pc.verifyParameters()
 
 request = pc.makeRequestRSpec()
 
-# Add a NUC eNB node
+# Add first NUC eNB node
 enb1 = request.RawPC("enb1")
 enb1.hardware_type = GLOBALS.NUC_HWTYPE
 enb1.disk_image = GLOBALS.UBUNTU_1804_LOW_LATENCY_IMG
 enb1.Desire("rf-controlled", 1)
 enb1_rue1_rf = enb1.addInterface("rue1_rf")
+
+# Add second NUC eNB node
+enb2 = request.RawPC("enb2")
+enb2.hardware_type = GLOBALS.NUC_HWTYPE
+enb2.disk_image = GLOBALS.UBUNTU_1804_LOW_LATENCY_IMG
+enb2.Desire("rf-controlled", 1)
+enb2_rue1_rf = enb2.addInterface("rue1_rf")
 
 # Add a NUC UE node
 rue1 = request.RawPC("rue1")
@@ -84,18 +92,31 @@ rue1.hardware_type = GLOBALS.NUC_HWTYPE
 rue1.disk_image = GLOBALS.UBUNTU_1804_LOW_LATENCY_IMG
 rue1.Desire("rf-controlled", 1)
 rue1_enb1_rf = rue1.addInterface("enb1_rf")
+rue1_enb2_rf = rue1.addInterface("enb2_rf")
 
-# Create the RF link between the UE and eNodeB
-rflink = request.RFLink("rflink")
-rflink.addInterface(enb1_rue1_rf)
-rflink.addInterface(rue1_enb1_rf)
+# Create the RF link between the UE and eNodeB #1
+rflink1 = request.RFLink("rflink")
+rflink1.addInterface(enb1_rue1_rf)
+rflink1.addInterface(rue1_enb1_rf)
 
-link = request.Link("lan")
-link.addNode(rue1)
-link.addNode(enb1)
-link.link_multiplexing = True
-link.vlan_tagging = True
-link.best_effort = True
+# Create the RF link between the UE and eNodeB #2
+rflink2 = request.RFLink("rflink")
+rflink2.addInterface(enb2_rue1_rf)
+rflink2.addInterface(rue1_enb2_rf)
+
+link1 = request.Link("lan")
+link1.addNode(rue1)
+link1.addNode(enb1)
+link1.link_multiplexing = True
+link1.vlan_tagging = True
+link1.best_effort = True
+
+link2 = request.Link("lan")
+link2.addNode(rue1)
+link2.addNode(enb2)
+link2.link_multiplexing = True
+link2.vlan_tagging = True
+link2.best_effort = True
 
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN, tourDescription)
