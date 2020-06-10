3ce99c1c60cbe2a1d73dd71b269b7aab87381fb5
prabodhmishra <35931371+prabodhmishra@users.noreply.github.com>
2020-06-10 01:52:13 -0400

    Add files for tracer usage

---
+++ bin/start_tracer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Run appropriate setup script
+
+NODE_ID=$(geni-get client_id)
+
+if [ $NODE_ID = "rue1" ]; then
+    /local/repository/bin/start-ue-tracer.sh
+elif [ $NODE_ID = "enb1" ]; then
+    /local/repository/bin/start-enb-tracer.sh
+else
+    echo "no setup necessary"
+fi
