67dbe7b8569ffb26c6645a408c25a74b4e8280b3
Prabodh Mishra <pmishra@clemson.edu>
2020-06-25 17:41:39 -0400

    test

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@ tourDescription = """
 
 This profile allocates hardware resources for deploying an OAI eNB and UE in
 **noS1** mode (no core network) in a controlled RF environment on the POWDER
-platform. The inlcuded scripts automate node setup, building the OAI binaries,
+platform. The included scripts automate node setup, building the OAI binaries,
 and starting the nework. The steps followed in these scripts are largely based
 on
 [this](https://gitlab.eurecom.fr/oai/openairinterface5g/wikis/HowToConnectOAIENBWithOAIUEWithoutS1Interface)
